944e7dff5143b0b3bf4444e60f487b9e33744bdb
Fundorin174 <Fundorin174@gmail.com>
2020-12-17 16:00:08 +0300

    настроил сокеты

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const app = require('express')(),
+  http = require('http').createServer(app),
+  io = require('socket.io')(http),
+  port = 3001;
 
 app.get('/', (request, response) => {
-    response.send(`<div style = "width: 100%; display: flex; justify-content: center">
-    <button style = "width:100px; height: 50px; padding: 10px; border-radius: 25px;" onclick = "handleClick()">Push me</button>
-    </div>
-    <script>
-    var handleClick = ()=>{
-        console.log("+");
-            fetch("/click", {method: 'POST'}).then((response)=>{
-                let result = response;
-                console.log(result)
-            })
-        }
-    </script>`)
+  response.sendFile(__dirname + '/index.html')
 })
 
 app.post('/click', function (request, response) {
-    console.log("clicked")
-    response.send('clicked');
-  });
+  console.log("clicked via Express");
+});
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-    console.log(`server is listening on ${port}`)
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    socket.emit('chat message', "ответка себе");
+    socket.broadcast.emit('chat message', "широковещательная ответка");
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
+http.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log(`server is listening on ${port}`)
 })
+
+
